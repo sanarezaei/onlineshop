a564973afb40eb66586ce402f55fc76907b6514b
= <sanare1382@gmail.com>
2024-07-27 15:01:11 +0330

    add pages app with HomePageView and AboutUsPageView

---
+++ pages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import HomePageView, AboutUsPageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name="home"),
+    path('aboutus/', AboutUsPageView.as_view(), name="aboutus")
+]
